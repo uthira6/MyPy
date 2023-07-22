a14bdff303a8a1648decacd901c60ce4d8475abf
uthira6 <111329006+uthira6@users.noreply.github.com>
2023-07-22 01:48:07 -0400

    Initial code

---
+++ Find_a_number.py
@@ -0,0 +1,14 @@
+
+x=int(input("enter a number less than ten: "))
+if x==1:
+    print("one")
+elif x==2:
+    print("two")
+elif x==3:
+    print("three")
+elif x==4:
+    print("four")
+elif x==5:
+    print("five")
+else :
+    print("enter different number!")
